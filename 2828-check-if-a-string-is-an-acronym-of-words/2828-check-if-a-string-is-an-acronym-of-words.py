cd1570dd8916c9446f7d07597329b2b619aefac9
Abhishek Misra <95607023+AbHIsHEK4011@users.noreply.github.com>
2023-08-22 23:22:13 +0530

    Time: 34 ms (29.13%), Space: 13.5 MB (30.81%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.py
@@ -5,11 +5,7 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l=[]
+        l=""
         for i in words:
-            l.append(i[0])
-        p=''.join(l)
-        if p==s:
-            return True
-        else:
-            return False
+            l+=i[0]
+        return l==s
